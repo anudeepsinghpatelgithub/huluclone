5208672fdac2b9c4048c435bfb684e69a77e6928
Anudeep Patel <anudeepsinghpatel@gmail.com>
2021-05-30 22:51:39 +0530

    home page done

--- components/Nav.js
+++ components/Nav.js
@@ -15,7 +15,7 @@ function Nav() {
           </h2>
         ))}
       </div>
-      <div className="absolute top-0 right-0 bg-gradient-to-l from-red-400 h-10 w-1/12" />
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12" />
     </nav>
   );
 }
